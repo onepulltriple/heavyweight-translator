3eda980d8c43d1574163325e442bd2c09bb09af9
Chase Helgenlechner <chase@helgenlechner.com>
2025-11-09 16:00:36 +0100

    Minor changes to file path handling to make "Copy as path" easier to use.

--- file_paths.py
+++ file_paths.py
@@ -23,25 +23,23 @@ results_history_folder_path             = document_components_folder_path + "/" 
 
 ###########################################################################
 # FILE PATHS
-#source_document_path                   = "./private/" + IP.source_document_file_name_without_extension + ".docx"
-source_document_path                   = IP.full_path_to_source_document
-#output_document_path                   = "./private/" + IP.source_document_file_name_without_extension + "__" + IP.operation_date + "_" + IP.target_lang_cult + ".docx"
-output_document_path_with_datetime     = results_history_folder_path + "/" + source_document_file_name_no_extension + "__" + IP.operation_datetime + "_" + IP.target_lang_cult + ".docx"
-output_document_path                   = IP.full_path_to_output_folder + "\\" + source_document_file_name_no_extension + "__" + IP.target_lang_cult + ".docx"
+source_document_path                    = IP.full_path_to_source_document.replace("\\","/")
+output_document_path_with_datetime      = results_history_folder_path + "/" + source_document_file_name_no_extension + "__" + IP.operation_datetime + "_" + IP.target_lang_cult + ".docx"
+output_document_path                    = IP.full_path_to_output_folder.replace("\\","/") + "/" + source_document_file_name_no_extension + "__" + IP.target_lang_cult + ".docx"
 
 dynamic_file_path_names = {
     "console_log_extraction_file_path":  console_logs_folder_path + "/" + IP.operation_datetime + "__" + constants.EXTRACT + "_step_for_" + IP.target_lang_cult + ".log",
     "console_log_swapping_file_path":    console_logs_folder_path + "/" + IP.operation_datetime + "__" + constants.SWAP    + "_step_for_" + IP.target_lang_cult + ".log"
 }
 
-source_language_plain_texts_file_path  = source_languages_folder_path    + "/" + IP.operation_date + "__extracted_source_text_elements__" + IP.source_lang_cult + ".csv"
-target_language_translations_file_path = target_languages_folder_path    + "/" + IP.operation_date + "__translated_text_elements__"       + IP.target_lang_cult + ".csv"
-TEMP_translation_dict_file_path        = pre_swapping_parts_folder_path  + "/" + IP.operation_date + "__TEMP_translation_dict__"          + IP.target_lang_cult + ".json"
-preprocessed_translations_file_path    = pre_swapping_parts_folder_path  + "/" + IP.operation_date + "__preprocessed_text_elements__"     + IP.target_lang_cult + ".csv"
+source_language_plain_texts_file_path   = source_languages_folder_path    + "/" + IP.operation_date + "__extracted_source_text_elements__" + IP.source_lang_cult + ".csv"
+target_language_translations_file_path  = target_languages_folder_path    + "/" + IP.operation_date + "__translated_text_elements__"       + IP.target_lang_cult + ".csv"
+TEMP_translation_dict_file_path         = pre_swapping_parts_folder_path  + "/" + IP.operation_date + "__TEMP_translation_dict__"          + IP.target_lang_cult + ".json"
+preprocessed_translations_file_path     = pre_swapping_parts_folder_path  + "/" + IP.operation_date + "__preprocessed_text_elements__"     + IP.target_lang_cult + ".csv"
 
-preprocessing_dict_file_path           = maintainable_parts_folder_path  + "/" + "PREPROCESSING_dict.json"
-FULL_translation_dict_file_path        = maintainable_parts_folder_path  + "/" + "MAINTAINED_translation_dict.json"
+preprocessing_dict_file_path            = maintainable_parts_folder_path  + "/" + "PREPROCESSING_dict.json"
+FULL_translation_dict_file_path         = maintainable_parts_folder_path  + "/" + "MAINTAINED_translation_dict.json"
 
 # Set up file path to dump unparseable xml strings
-start_of_xml_debug_file_path           = console_logs_folder_path + "/" + IP.operation_datetime + "__unparseables/"
-end_of_xml_debug_file_path             = "__xml_debug_" + IP.target_lang_cult + ".xml"
+start_of_xml_debug_file_path            = console_logs_folder_path + "/" + IP.operation_datetime + "__unparseables/"
+end_of_xml_debug_file_path              = "__xml_debug_" + IP.target_lang_cult + ".xml"
--- input_parameters.py
+++ input_parameters.py
@@ -2,28 +2,28 @@ from datetime import datetime
 
 #__________________________________________________________________________
 ###########################################################################
-# Variables
-#source_document_file_name_without_extension = "translogica Benutzerhandbuch New Design"
-#source_document_file_name_without_extension = "19.09.2025__translogica Benutzerhandbuch"
-#source_document_file_name_without_extension = "BeispielTDB"
+# VARIABLES
+# Enter a full or relative file path preceded by 'r' and wrapped in double quotes, for example:
+# path_to_source_document = r"N:\example_output_folder_name\subfolder_name\Admin_Datenbankstruktur.docx"
+# path_to_output_folder   = r"N:\example_output_folder_name\subfolder_name"
+# In Microsoft Windows, Shift + Right-click on source document, then select 'Copy as path'
+path_to_source_document = r"N:\test_zentralen\test_Handbuch für Administratoren.docx"
+path_to_output_folder   = r"N:\test_zentralen\filialen"
 
-#full_path_to_source_document = "N:\\test_zentralen\\filialen\\Admin_Datenbankstruktur.docx"
-full_path_to_source_document = "C:\\Users\\Chase Helgenlechner\\Documents\\00__Coding Projects\\01__Translation Projects\\03__heavyweight-translator\\private\\Anwendungsfall.docx"
-#full_path_to_output_folder   = "N:\\test_zentralen\\filialen"
-full_path_to_output_folder   = "C:\\Users\\Chase Helgenlechner\\Downloads\\__target folder"
-relative_path_to_source_document = None
-
-operation_datetime = datetime.today().strftime('%Y.%m.%d %H.%M') # https://docs.python.org/3/library/time.html#time.strftime
-operation_date = datetime.today().strftime('%Y.%m.%d')
-#operation_date = "2025.10.19" # yyyy.MM.dd
-
-
-percentage_increment_to_report = 1 #percent
-
+# Set the language of the source document
 source_language = "de"
 source_culture = "DE"
 source_lang_cult = source_language + "-" + source_culture
 
+# Set the language of the target document
 target_language = "en"
 target_culture = "GB"
 target_lang_cult = target_language + "-" + target_culture
+
+# Set the progress reporting frequency (prints progress to console after every % increment completed)
+percentage_increment_to_report = 1 
+
+# Operation date and time (comment in last line to set manually)
+operation_datetime = datetime.today().strftime('%Y.%m.%d %H.%M') # https://docs.python.org/3/library/time.html#time.strftime
+operation_date     = datetime.today().strftime('%Y.%m.%d')
+#operation_date     = "2025.10.19" # yyyy.MM.dd
